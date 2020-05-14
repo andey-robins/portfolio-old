7a5da832a1169eaa423fe5b2c3df964d9ad93124
Andey <andeyrobins@gmail.com>
2020-05-14 17:39:56 -0600

    Convert Text Adventure to Object Oriented Approach

--- text-adventure/game.js
+++ text-adventure/game.js
@@ -1,13 +1,3 @@
-const house = [
-    "bedroom", "den", "storage",
-    "library", "courtyard", "garage",
-    "game", "kitchen", "foyer"
-]
-
-// player values
-var position = "courtyard";
-var inventory = [];
-
 // this is the entry point from the html application and the place that processes game actions
 function actOnUserInput() {
     // get the user input from the input box
@@ -17,81 +7,105 @@ function actOnUserInput() {
 
     console.log(commandList);   // debugging output
 
-    if (commandList[0] == "go") {
-        // go to the room
-        if (house.includes(commandList[1])) {
-            console.log(gameObject.traversals[position]);
-            if (gameObject.traversals[position].includes(commandList[1])) {
-                dm = "You go to the " + commandList[1];
-                position = commandList[1];
+    //convert user information into game objects
+    let targetObject = getItemObjectFromName(commandList[1]);
+    let roomObject = roomNameMap[player.room];
 
-                // formatting for the game room
-                if (commandList[1] == "game") {
-                    dm += " room";
-                }
+    switch(commandList[0]) {
+        case "go":
+            // go to the room
+            if (house.includes(commandList[1])) {
+                dm += "You go to the " + commandList[1];
+                player.room = commandList[1].toString();
             } else {
-                dm = "You are unable to get to the " + commandList[1] + " from the " + position;
+                dm += "That is not a valid room."
             }
-        // failure case
-        } else {
-            dm = "That's not a valid room";
-        }
+            break;
 
-    } else if (commandList[0] == "take") {
-        // check to see if the object is in the room you're in
-        if (gameObject.roomInventories[position].includes(commandList[1])) {
-            inventory.push(commandList[1]);
-            gameObject.roomInventories[position] = removeFromArray(gameObject.roomInventories[position], commandList[1]);
-            dm = "You take the " + commandList[1];
-        // failure case
-        } else {
-            dm = "You can't take " + commandList[1];
-        }
+        case "take":
+            // check if the item is in the room you're in
+            if (roomObject.inventory.includes(targetObject)) {
+                player.inventory.push(targetObject);
+                roomObject.inventory = removeFromArray(roomObject.inventory, targetObject);
+                dm += "You take the " + targetObject.name;
+            } else {
+                dm += "You can't take that";
+            }
+            break;
 
-    } else if (commandList[0] == "put") {
+        case "put":
+            // check if the player has the item
+            if (player.inventory.includes(targetObject)) {
+                roomObject.inventory.push(targetObject);
+                player.inventory = removeFromArray(player.inventory, targetObject);
+                dm += "You put down the " + targetObject.name;
+            } else {
+                dm += "You don't have that item";
+            }
+            break;
 
+        case "look":
+            // allow general looking
+            if (commandList[1] == 'around') {
+                dm += roomObject.description;
+                roomObject.inventory.forEach((item) => {
+                    dm += '. ';
+                    dm += item.description;
+                });
+                dm.substring(0, dm.length - 2);
 
-    } else if (commandList[0] == "look") {
-        // a command to look at the room around you
-        if (commandList[1] == "around") {
-            dm = gameObject.descriptions[position];
+            // item is in the players inventory
+            } else if (player.inventory.includes(targetObject)) {
+                dm += targetObject.description;
 
-        // look at a specific object
-        // TODO: check to see if the object is in your inventory or in the room
-        } else if (gameObject.itemList.includes(commandList[1])) {
-            console.log(commandList[1]);
-            dm = gameObject.items[commandList[1].toString()].description;
+            // item is in the room
+            } else if (roomObject.inventory.includes(targetObject)) {
+                dm += targetObject.description;
 
-        // failure case
-        } else {
-            dm = "You don't see a " + commandList[1];
-        }
+            // failure case
+            } else {
+                dm += "You don't see that object";
+            }
+            break;
 
-    } else if (commandList[0] == "open") {
-        if (gameObject.openableObjects.includes(commandList[1])) {
-            dm = "You can open that object";
-        } else {
-            dm = "You can't open the " + commandList[1];
-        }
+        case "open":
+            // call into object actionMap to see if it has an open action
+            if (player.inventory.includes(targetObject) || roomObject.inventory.includes(targetObject)) {
+                let actionResponse = targetObject.actions("open", commandList[2]);
+                if (actionResponse != undefined) {
+                    dm += actionResponse;
+                } else {
+                    dm += "You cannot open that object";
+                }
+            }
+            break;
 
-    } else if (commandList[0] == "inventory") {
-        // print out the inventory
-        dm = getPrintableInventory();
+        case "inventory":
+            // print out the inventory
+            dm = getPrintableInventory();
+            break;
 
-    } else if (commandList[0] == "solve") {
-        if (gameObject.puzzles[commandList[1]] == commandList[2]) {
-            dm = gameObject.solutionText[commandList[1]];
-        } else {
-            dm = "You seem to be unable to solve the puzzle in that manner";
-        }
-    } else if (commandList[0] == "help") {
-        dm = "Enter a command into the text box below. You can do thinks like, look, take, put, or solve. Follow the verb with some object that you want to act on like 'look at lockbox', 'take book', or 'solve lockbox'. When solving, use the syntax 'solve $PUZZLE with $CODE'";
-    } else {
-        // final failure case
-        dm = "Unfortunately, I don't understand: " + userInput;
+        case "solve":
+            // call into object actionMap to see if it has a solve action
+            if (player.inventory.includes(targetObject) || roomObject.inventory.includes(targetObject)) {
+                let actionResponse = targetObject.actions("solve", commandList[2]);
+                if (actionResponse != undefined) {
+                    dm += actionResponse;
+                } else {
+                    dm += "You cannot solve that object";
+                }
+            }
+            break;
+
+        case "help":
+            dm = "Enter a command into the text box below. You can do thinks like, look, take, put, or solve. Follow the verb with some object that you want to act on like 'look at lockbox', 'take notepad', or 'solve lockbox'. When solving, use the syntax 'solve $PUZZLE with $CODE'";
+            break;
+
+        default:
+            print("I'm sorry, but I don't understand: " + userInput.toString());
     }
 
-    // these should be the final three lines of this function
+    // these should be the final four lines of this function
     // they format and write out the DM response and then clear out the input box
     dm += "."
     print(dm);
@@ -110,7 +124,7 @@ function cleanInput(userInput) {
 
     // parse commands and convert them to our action language
     // parse the verb
-    command[0] = gameObject.verbs[command[0]];
+    command[0] = verbs[command[0]];
 
     // return parsed command list
     return command;
@@ -118,12 +132,12 @@ function cleanInput(userInput) {
 
 // return a printable version of the players inventory
 function getPrintableInventory() {
-    if (inventory.length != 0) {
+    if (player.inventory.length != 0) {
         var inv = "You have: ";
-        for (var item in inventory) {
-            inv += inventory[item];
+        player.inventory.forEach(item => {
+            inv += item.name;
             inv += ", ";
-        }
+        });
         return inv.substring(0, inv.length - 2);
     } else {
         return "You have no items";
@@ -142,79 +156,178 @@ function removeFromArray(arr, it) {
     });
 }
 
-// the text content of the game stored within an object for reference
-var gameObject = {
-    "descriptions": {
-        "bedroom": "The bedroom has a bed in the center. On the wall is a painting of a woman singing. The bed has a red comforter laid across it. To the east is the den and to the south is the library",
-        "den": "The den has a fireplace on the north wall. In the center of the room is a comfy looking couch. A knife and a block of cheese sits on a silver plate on the couch. To the east is the box filled storage room, to the south is the courtyard and fountain, and to the west is the bedroom",
-        "storage": "The storage room has a large number of boxes in it. The boxes are labeled 'spoons,' 'knives,' and 'forks.' To the south is the garage and to the west is the den",
-        "library": "The library has lots of books in it. In the center of the room is an oak desk. On the desk sits a book, a notepad, and a pen. To the north is a doorway to a bedroom, to the east is a courtyard with a fountain, and to the south is a game room",
-        "courtyard": "In the middle of the couryard is a large fountain. You can see a lockbox sitting on the edge of the fountain. Around the edge of the courtyard are a number of benches, chairs, and side-tables. The sky is open, and thousands of stars dot the sky. To the north is a doorway that opens to a den. To the east is what looks to be a garage. To the south is a kitchen, and to the west is a library",
-        "garage": "In the garage is a pair of bycicles. Hanging from the back of one bike is a license plate. Between the handlebars of the other bike is a piece of paper. The room is dim, and there is no sign of anything else in the light. TO the north is the storage room, to the south is the foyer, and to the west is the courtyard",
-        "game": "There is a table in the middle of the room and shelves with games along the walls. On the table is a chess board",
-        "kitchen": "On the kitchen counter are three scales. Each scale is labeled with a sticky note that has a drawing of either a fork, a knife, or a spoon",
-        "foyer": "In the foyer there is a door to the outside world. Along one wall is a pair of boots and a coat on a coat hanger. A welcome mat sits in front of the door"
-    },
+function getItemObjectFromName(name) {
+    let foundObject = null;
+    items.forEach(item => {
+        if (item.name == name) {
+            foundObject = item;
+        }
+    });
+    return foundObject;
+}
 
-    "verbs": {
-        "go": "go",
-        "move": "go",
-        "travel": "go",
-        "walk": "go",
-        "take": "take",
-        "pick": "take",
-        "get": "take",
-        "put": "put",
-        "place": "put",
-        "set": "put",
-        "look": "look",
-        "check": "look",
-        "open": "open",
-        "inventory": "inventory",
-        "solve": "solve",
-        "help": "help"
-    },
-
-    "roomInventories": {
-        "bedroom": [],
-        "den": [],
-        "storage": [],
-        "library": [],
-        "courtyard": ["lockbox", "key"],
-        "garage": [],
-        "game": [],
-        "kitchen": [],
-        "foyer": []
-    },
-
-    "items": {
-        "lockbox": { "description": "The lockbox is made of glass with a combination lock on the front. Six numbers need to be input to the lock. Within the box, a key is visible", "canTake": false },
-        "key": { "description": "Within the box, there is a stainless-steel house key. There is nothing remarkable about it", "canTake": false },
-        "chairs": { "description": "The chairs are simple, but comfortable looking. You could sit down, but you should keep looking around", "canTake": false },
-        "stars": { "description": "The stars twinkle in the sky above you. There's no telling what secrets they may hold. Oh well, you should continue on", "canTake": false }
-    },
-
-    "itemList": ["lockbox", "key", "chairs", "stars"],
-
-    "puzzles": {
-        "lockbox": "138142"
-    },
-
-    "solutionText": {
-        "lockbox": "You open up the lockbox. You can now take the key"
-    },
-
-    "openableObjects": [],
-
-    "traversals": {
-        "bedroom": ["den", "library"],
-        "den": ["bedroom", "storage", "courtyard"],
-        "storage": ["den", "garage"],
-        "library": ["bedroom", "courtyard", "game"],
-        "courtyard": ["den", "garage", "kitchen", "library"],
-        "garage": ["storage", "foyer", "courtyard"],
-        "game": ["library", "kitchen"],
-        "kitchen": ["courtyard", "foyer", "game"],
-        "foyer": ["garage", "kitchen"]
+//
+// object declarations
+//
+class Room {
+    constructor(description, actionMap, inventory) {
+        this.description = description;
+        this.actions = actionMap;
+        this.inventory = inventory;
     }
 }
+
+class Item {
+    constructor(id, name, description, actionMap) {
+        this.itemId = id;
+        this.name = name;
+        this.description = description;
+        this.actions = actionMap;
+    }
+}
+
+class Player {
+    constructor() {
+        this.inventory = new Array();
+        this.room = 'courtyard';
+    }
+}
+
+const verbs = {
+    "go": "go",
+    "move": "go",
+    "travel": "go",
+    "walk": "go",
+    "take": "take",
+    "pick": "take",
+    "get": "take",
+    "put": "put",
+    "place": "put",
+    "set": "put",
+    "look": "look",
+    "check": "look",
+    "open": "open",
+    "inventory": "inventory",
+    "solve": "solve",
+    "help": "help"
+}
+
+// create a player object
+let player = new Player();
+
+//
+// define items
+//
+let libraryBook = new Item('libraryBook',
+                        "book",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let libraryNotepad = new Item('libraryNotepad',
+                        "notepad",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let libraryPen = new Item('libraryPen',
+                        "pen",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let courtyardLockbox = new Item('courtyardLockbox',
+                        "lockbox",
+                        "description",
+                        (action, options) => {
+                            switch (action) {
+                                case 'solve':
+                                    if (options == 138142) {
+                                        player.inventory.push(courtyardKey);
+                                        return "You open the lockbox and take the key";
+                                     } else {
+                                        return "Nothing happens";
+                                     }
+                                    break;
+                                default:
+                                    return undefined;
+                            }
+                        });
+
+let courtyardKey = new Item('courtyardKey',
+                        "key",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let courtyardSpoon = new Item('courtyardSpoon',
+                        "spoon",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let denChessBoard = new Item('denChessBoard',
+                        "chessboard",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let denGames = new Item('denGames',
+                        "games",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let kitchenScale = new Item('kitchenScale',
+                        "scale",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let kitchenStickyNote = new Item('kitchenStickyNote',
+                        "note",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let foyerBoots = new Item('foyerBoots',
+                        "boots",
+                        "description",
+                        (action, options) => {return undefined;});
+
+let foyerCoat = new Item('foyerCoat',
+                        "coat",
+                        "description",
+                        (action, options) => {return undefined;});
+
+//
+// define rooms
+//
+let library = new Room("The library has lots of books in it. In the center of the room is an oak desk. On the desk sits a book, a notepad, and a pen. To the north is a doorway to a bedroom, to the east is a courtyard with a fountain, and to the south is a game room",
+                        (action) => {print(action)},
+                        [libraryBook, libraryNotepad, libraryPen]);
+
+let courtyard = new Room("In the middle of the couryard is a large fountain. You can see a lockbox sitting on the edge of the fountain. Around the edge of the courtyard are a number of benches, chairs, and side-tables. The sky is open, and thousands of stars dot the sky. To the north is a doorway that opens to a den. To the east is what looks to be a garage. To the south is a kitchen, and to the west is a library",
+                        (action) => {print(action)},
+                        [courtyardLockbox, courtyardSpoon]);
+
+let den = new Room("There is a table in the middle of the room and shelves with games along the walls. On the table is a chess board",
+                        (action) => {print(action)},
+                        [denChessBoard, denGames]);
+
+let kitchen = new Room("On the kitchen counter are three scales. Each scale is labeled with a sticky note that has a drawing of either a fork, a knife, or a spoon",
+                        (action) => {print(action)},
+                        [kitchenScale, kitchenStickyNote]);
+
+let foyer = new Room("In the foyer there is a door to the outside world. Along one wall is a pair of boots and a coat on a coat hanger. A welcome mat sits in front of the door",
+                        (action) => {print(action)},
+                        [foyerBoots, foyerCoat]);
+
+// a map to look up the room object by name
+const roomNameMap = {
+    'library': library,
+    'courtyard': courtyard,
+    'den': den,
+    'kitchen': kitchen,
+    'foyer': foyer,
+}
+
+// list of rooms
+const rooms = ['library', 'courtyard', 'den', 'kitchen', 'foyer'];
+// other items may exist, but these are the ones that can be picked up moved about and interacted with by the player
+const items = [libraryBook, libraryNotepad, libraryPen,
+                courtyardLockbox, courtyardSpoon,
+                denChessBoard, denGames,
+                kitchenScale, kitchenStickyNote,
+                foyerBoots, foyerCoat];
